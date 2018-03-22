2d1e2713bd15edb221ff9492b4565739ae626943
ariel <ariel@Ariel-EB>
2018-03-22 09:09:46 +0700

    tes

--- TugasBesar1573004-1573007-ejb/src/java/entitas/DetailOrder.java
+++ TugasBesar1573004-1573007-ejb/src/java/entitas/DetailOrder.java
@@ -28,54 +28,16 @@ public class DetailOrder implements Serializable {
     @ManyToOne
     @JoinColumn(name = "idOrder")
     private Order1 idOrder;
-    
+
     @OneToOne(cascade = CascadeType.ALL)
     private Book id;
-    
-    private Float hargaBeli;
+
+    private int subtotal;
+
+    private int jumlah;
 
     public DetailOrder() {
     }
 
-    public DetailOrder(Long idDetailOrder, Order1 idOrder, Book id, Float hargaBeli) {
-        this.idDetailOrder = idDetailOrder;
-        this.idOrder = idOrder;
-        this.id = id;
-        this.hargaBeli = hargaBeli;
-    }
-
-    public Long getIdDetailOrder() {
-        return idDetailOrder;
-    }
-
-    public void setIdDetailOrder(Long idDetailOrder) {
-        this.idDetailOrder = idDetailOrder;
-    }
-
-    public Order1 getIdOrder() {
-        return idOrder;
-    }
-
-    public void setIdOrder(Order1 idOrder) {
-        this.idOrder = idOrder;
-    }
-
-    public Book getId() {
-        return id;
-    }
-
-    public void setId(Book id) {
-        this.id = id;
-    }
-
-    public Float getHargaBeli() {
-        return hargaBeli;
-    }
-
-    public void setHargaBeli(Float hargaBeli) {
-        this.hargaBeli = hargaBeli;
-    }
     
-    
-
 }
